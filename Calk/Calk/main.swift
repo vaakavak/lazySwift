86b47a5c51bace73a60cd513d6755e8f99a439ee
vaakavak <32431682+vaakavak@users.noreply.github.com>
2023-12-04 09:16:23 +0400

    fix

--- Calk/Calk/main.swift
+++ Calk/Calk/main.swift
@@ -6,25 +6,7 @@
 //
 
 import Foundation
-
-print("Добро пожаловать в программу калькулятор")
-
-
-let operation = getDataFromUser(description: "Выберите операцию: +, -, *, /")
-let firstNumber = getDataFromUser(description: "Введите целое число")
-let secondNumber = getDataFromUser(description: "Введите второе число")
-
-print("Идет вычисление примера: " + firstNumber + " " + operation + " " + secondNumber)
-
-if let firstNumber = Int(firstNumber) {
-    if let secondNumber = Int(secondNumber) {
-        culculate(opertion: operation, firstNumber: firstNumber, secondNumber: secondNumber)
-    } else {
-        print("Вы вели не верное первое число")
-    }
-} else {
-    print("Вы ввели не верное первое число")
-}
+import Darwin
 
 func getDataFromUser(description: String) -> String {
     print(description)
@@ -32,10 +14,42 @@ func getDataFromUser(description: String) -> String {
 }
 
 func showResult(_ result: Int) {
-    let result = String(result)
-    let description = "Результат:"
-    print(description + " " + result)
+    print("Результат: " + String(result))
 }
+
+print("Добро пожаловать в программу калькулятор")
+
+while true {
+    let operation = getDataFromUser(description: "Выберите операцию: +, -, *, /. Для завершения работы введите q.")
+    if operation == "q" {
+        exit(0)
+    }
+    let firstNumber = getDataFromUser(description: "Введите целое число")
+    let secondNumber = getDataFromUser(description: "Введите второе число")
+
+    print("Идет вычисление примера: " + firstNumber + " " + operation + " " + secondNumber)
+
+    if let firstNumber = Int(firstNumber) {
+        if let secondNumber = Int(secondNumber) {
+            culculate(opertion: operation, firstNumber: firstNumber, secondNumber: secondNumber)
+        } else {
+            print("Вы вели не верное первое число")
+        }
+    } else {
+        print("Вы ввели не верное первое число")
+    }
+    
+    print("")
+    print("--------------------------------------------")
+    print("")
+}
+
+
+
+
+
+
+
 
 func culculate (opertion: String, firstNumber: Int, secondNumber: Int ) {
     switch operation {
