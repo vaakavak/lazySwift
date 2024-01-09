526115eafece81c03e7f3ab4e0c4c682eceb7ecb
vaakavak <32431682+vaakavak@users.noreply.github.com>
2024-01-09 20:16:52 +0400

    create func

--- LazySwiftCycleAndArray/LazySwiftCycleAndArray/main.swift
+++ LazySwiftCycleAndArray/LazySwiftCycleAndArray/main.swift
@@ -7,10 +7,18 @@
 
 import Foundation
 
-var a = [Int]()
-
-for _ in 0..<10 {
-    a.append(Int.random(in: 0..<100))
-}
+//var a = [Int]()
+//
+//for _ in 0..<10 {
+//    a.append(Int.random(in: 0..<100))
+//}
 
 //print(a)
+
+func makeArray() -> [Int] {
+    var array = [Int]()
+    for _ in 0..<10 {
+        array.append(Int.random(in: 0..<100))
+    }
+    return array
+}
