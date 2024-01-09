7a35698ac9543173e2770f0f80f25b5b56f1025d
vaakavak <32431682+vaakavak@users.noreply.github.com>
2024-01-09 20:12:53 +0400

    add file homeWork

--- ForLesson/ForLesson/main.swift
+++ ForLesson/ForLesson/main.swift
@@ -8,4 +8,4 @@
 import Foundation
 
 print("Hello, World!")
-
+let person: (name1: String, age: Int) = (name1: "Serofim", age: 32)
---
+++ LazySwiftCycleAndArray/LazySwiftCycleAndArray/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  LazySwiftCycleAndArray
+//
+//  Created by Евгений Шилов on 09.01.2024.
+//
+
+import Foundation
+
+var a = [Int]()
+
+for _ in 0..<10 {
+    a.append(Int.random(in: 0..<100))
+}
+
+//print(a)
