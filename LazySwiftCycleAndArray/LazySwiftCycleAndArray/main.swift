99baa1e178540862ad4d42f70b36492b9b98280f
vaakavak <32431682+vaakavak@users.noreply.github.com>
2024-01-09 20:51:50 +0400

    finish first task

--- LazySwiftCycleAndArray/LazySwiftCycleAndArray/main.swift
+++ LazySwiftCycleAndArray/LazySwiftCycleAndArray/main.swift
@@ -12,13 +12,34 @@ import Foundation
 //for _ in 0..<10 {
 //    a.append(Int.random(in: 0..<100))
 //}
-
 //print(a)
+
+
+////  1) Найти максимальный элемент
+//let a = makeArray()
+//var maximumNumber = a[0] //для сравнения беру первый элемент массива
+//
+//for elements in a {              //беру каждый элемент из массива
+//    if elements > maximumNumber{ //сравниваю его с переменной maximumNumber
+//        maximumNumber = elements //и если max больше, кладу туда элемент массива
+//    }
+//}
+//print(a)
+//print(maximumNumber)
+
+
+
+// 2) Возведение значений в массиве в квадрат
+
+
+
+
+
 
 func makeArray() -> [Int] {
     var array = [Int]()
     for _ in 0..<10 {
-        array.append(Int.random(in: 0..<100))
+        array.append(Int.random(in: -100..<(-10)))
     }
     return array
 }
