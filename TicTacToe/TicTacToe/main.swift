5b506f38b5b0a8ea719f22d722bc53c7f65de70e
vaakavak <32431682+vaakavak@users.noreply.github.com>
2024-01-20 14:20:27 +0400

    created project TicTacToe

---
+++ TicTacToe/TicTacToe/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  TicTacToe
+//
+//  Created by Евгений Шилов on 20.01.2024.
+//
+
+import Foundation
+
+let symbols = (
+    empty: " ",
+    player1: "X",
+    player2: "O"
+)
+
+print("Добро пожаловать в игру крестики нолики")
+while true {
+    
+}
