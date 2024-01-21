af4249c5391ac196c02e73047a54ac48be503405
vaakavak <32431682+vaakavak@users.noreply.github.com>
2024-01-21 18:45:12 +0400

    created func printField

--- TicTacToe/TicTacToe/main.swift
+++ TicTacToe/TicTacToe/main.swift
@@ -15,5 +15,69 @@ let symbols = (
 
 print("Добро пожаловать в игру крестики нолики")
 while true {
+    let playerName1 = getDataFromUser(description: "Введите имя первого игрока")
+    guard !playerName1.isEmpty else {
+        print("Вы ввели неверное имя первого игрока")
+        continue
+    }
+    let playerName2 = getDataFromUser(description: "Введите имя второго игрока")
+    guard !playerName2.isEmpty else {
+        print("Вы ввели неверное имя второго игрока")
+        continue
+    }
+    let fieldSize = getDataFromUser(description: "Введите размер игрового поля")
+    guard let fieldSize = Int(fieldSize), fieldSize > 0 else {
+        print("Вы ввели неверный размер игрового поля")
+        continue
+    }
     
+    var field = [[String]]()
+    for _ in 0..<fieldSize {
+        var row = [String]()
+        for _ in 0..<fieldSize {
+            row.append(symbols.empty)
+        }
+        field.append(row)
+    }
+    
+    var fieldFormatedString = ""
+    for row in field {
+        fieldFormatedString += "|"
+        for cell in row {
+            fieldFormatedString += cell + "|"
+        }
+        fieldFormatedString += "\n"
+        for _ in 0..<(fieldSize * 2 + 1) {
+            fieldFormatedString += "-"
+        }
+        fieldFormatedString += "\n"
+    }
+    print(fieldFormatedString)
+    
+    print("\(playerName1) Делает ход")
+    let row = getDataFromUser(description: "Введите номер строки")
+    guard let row = Int(row), row >= 0, row < fieldSize else {
+        print("Введен не верный номер строки")
+        continue
+    }
+    let column = getDataFromUser(description: "Введите номер столбца")
+    guard let column = Int(column), column >= 0, column < fieldSize else {
+        print("Введен не верный номер столбца")
+        continue
+    }
+    field[row][column] = symbols.player1
+    
+    
+    
+    readLine()
+}
+
+func printField() {
+    
+}
+
+
+func getDataFromUser(description: String) -> String {
+    print(description)
+    return readLine() ?? " "
 }
